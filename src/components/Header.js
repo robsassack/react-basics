de25dd564d6927cdf6a6cbeea8067b5b303c6a68
Rob Sassack <rsassack25@gmail.com>
2022-10-14 23:49:00 -0400

    Fixed link

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export class Header extends React.Component {
         <div className="container">
           <div className="navbar-header">
             <ul className="nav navbar-nav">
-              <li><a href="#">Home</a></li>
+              <li><a href="/#">Home</a></li>
             </ul>
           </div>
         </div>
