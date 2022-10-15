24a9d08d6a4d45108dff535c78fbdeb4c5d45fd4
Rob Sassack <rsassack25@gmail.com>
2022-10-15 16:37:08 -0400

    Added props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,15 +3,34 @@ import React, { Component } from 'react';
 class MyComponent extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+    };
+
+    this.countUp = this.countUp.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
+
+  countUp() {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
+  countDown() {
+    this.setState({
+      count: this.state.count - 1,
+    })
   }
 
   render() {
-    const { title, onButtonClicked } = this.props;
-
     return (
       <div>
-        <h1>{ title }</h1>
-        <button onClick={ onButtonClicked }>Click here</button>
+        <h1>{ this.props.title }</h1>
+        <button onClick={ this.countUp }>Count Up</button>
+        <button onClick={ this.countDown }>Count Down</button>
+        <p>{ this.state.count }</p>
       </div>
     );
   }
